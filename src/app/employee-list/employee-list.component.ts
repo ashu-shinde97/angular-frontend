e2e6553a7bb27ec0a213aad8c674eae0e1d8f64f
ashu-shinde97 <shinde.ashutosh1997@gmail.com>
2025-02-07 20:31:10 +0530

    uploading fronted app for first time

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees: Employee[] = [];
+  searchText: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // const storedEmployees = localStorage.getItem('employees');
+  
+    // if (storedEmployees) {
+    //   // Load saved employees from localStorage if available
+    //   this.employees = JSON.parse(storedEmployees);
+    // } else {
+      // If no saved data, use the default
+      this.employees = [
+        { id: 1, firstname: 'ashutosh', emailid: 'ashu@gmail.com', Lastname: 'shinde' },
+        { id: 2, firstname: 'Rohit', emailid: 'rohit@gmail.com', Lastname: 'Patil' },
+        { id: 3, firstname: 'Vishal', emailid: 'Vishal@gmail.com', Lastname: 'Patil' }
+      ];
+    }
+  //}
+  filteredEmployees(): Employee[] {
+    if (!this.searchText.trim()) {
+      return this.employees;  // Return full list if search text is empty
+    }
+
+    return this.employees.filter(employee =>
+      employee.firstname.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      employee.Lastname.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
+
+  // saveEmployee(index: number): void {
+  //   const updatedEmployee = this.employees[index];
+  //   console.log(`Employee at index ${index} updated:`, updatedEmployee);
+    
+  //   // Save the updated employees array to localStorage
+  //   localStorage.setItem('employees', JSON.stringify(this.employees));
+  // }
+}
+
+
